a6cf9040ea1fccfeef47619bb57e09acb8ae557c
naiaden <louis@naiaden.nl>
2015-03-03 17:07:24 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -17,7 +17,8 @@ try:
     posts = json.load(open('posts.json'))
 except IOError, OSError:
     print 'File posts.json not found!'
-print 'Found {0} populated subreddits.'.format(len(posts))
+nr_posts = sum([len(posts[sr]) for sr in [s for s in posts]])
+print 'Found {0} populated subreddits with {1} posts.'.format(len(posts), nr_posts)
 
 for subreddit in subreddits:
     print 'Processing {0}'.format(subreddit)
@@ -31,10 +32,11 @@ for subreddit in subreddits:
         nr_submissions = nr_submissions + 1
         if submission.id not in posts[subreddit]:
             nr_submissions_added = nr_submissions_added + 1
-            posts[subreddit][submission.id] = submission.selftext
+            posts[subreddit][submission.id] = [submission.title, submission.selftext]
     print '    Added {0} of {1} submissions.'.format(nr_submissions_added, nr_submissions)
     print '    We now have {0} submissions for {1}.'.format(len(posts[subreddit]), subreddit)
 
-print 'In total we now have {0} subreddits, with {1} posts.'.format(len(posts), sum([len(posts[sr]) for sr in [s for s in posts]]))
+new_nr_posts = sum([len(posts[sr]) for sr in [s for s in posts]])
+print 'In total we now have {0} subreddits, with {1} posts ({2} new).'.format(len(posts), new_nr_posts, new_nr_posts-nr_posts)
 
 json.dump(posts, open("posts.json", 'w'))
